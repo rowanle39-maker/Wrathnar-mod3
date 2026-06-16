82fd2b61bad5a9b48364f6cb3d9edb9e8979b05c
rowanle39-maker <rowanle39@gmail.com>
2026-06-16 15:27:40 +0300

    Create WrathnarClientMod.java

---
+++ src/main/java/net/wrathnar/mod/WrathnarClientMod.java
@@ -0,0 +1,22 @@
+package net.wrathnar.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class WrathnarClientMod implements ClientModInitializer {
+
+	public static KeyBinding flyKey;
+
+	@Override
+	public void onInitializeClient() {
+		flyKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.wrathnar.fly",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_Z,
+				"category.wrathnar"
+		));
+	}
+}
